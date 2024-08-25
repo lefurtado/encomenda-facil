912c9485fb00d4f9ea168c4908fa54b088ab75e9
Leandro <le_furtado@outlook.com.br>
2024-08-25 19:14:43 -0300

    🔧fix: lógica pegando sigla do bloco no relacionamento

--- src/app/moradores/page.tsx
+++ src/app/moradores/page.tsx
@@ -13,12 +13,27 @@ import { DataTable } from "@/components/ui/data-table";
 import { columns } from "./columns";
 import { useQuery } from "react-query";
 import { getMoradores } from "@/services/moradores";
+import { getBlocos } from "@/services/blocos";
 
 export default function Moradores() {
   const { data: moradoresData, isLoading } = useQuery({
     queryFn: async () => await getMoradores(),
     queryKey: ["moradores"],
     refetchOnWindowFocus: false,
+  });
+
+  const { data: blocosData } = useQuery({
+    queryFn: async () => await getBlocos(),
+    queryKey: ["blocos"],
+    refetchOnWindowFocus: false,
+  });
+
+  const moradoresComSiglaBloco = moradoresData?.data?.map((morador) => {
+    const bloco = blocosData?.data?.find((b) => b.id == morador.idBloco);
+    return {
+      ...morador,
+      bloco: bloco ? bloco?.sigla : "",
+    };
   });
 
   if (isLoading) {
@@ -39,7 +54,7 @@ export default function Moradores() {
         </BreadcrumbList>
       </Breadcrumb>
       <DialogNewMorador />
-      <DataTable columns={columns} data={moradoresData?.data || []} />
+      <DataTable columns={columns} data={moradoresComSiglaBloco || []} />
     </div>
   );
 }
